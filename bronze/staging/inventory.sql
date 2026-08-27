8af1c244acf2be8f4b7ff49348ba30e5c2f26596
treesa maria cinil <150402070+treesacinil07@users.noreply.github.com>
2026-08-27 22:40:51 +0530

    Add files via upload

--- bronze/staging/inventory.sql
+++ bronze/staging/inventory.sql
@@ -1,12 +1,16 @@
-CREATE TABLE staging.suppliers (
-    Supplier_ID VARCHAR(20),
-    Supplier_Name VARCHAR(100),
-    Supplier_Type VARCHAR(50),
-    Supplier_Country VARCHAR(50),
-    Supplier_City VARCHAR(50),
-    Primary_Category VARCHAR(100),
-    Supplier_Rating VARCHAR(20),
-    Typical_Lead_Time_Days INT,
-    Supplier_Status VARCHAR(50),
-    Currency VARCHAR(10)
+CREATE TABLE staging.inventory (
+    Inventory_ID VARCHAR(20),
+    PO_ID VARCHAR(20),
+    Product_ID VARCHAR(20),
+    Warehouse_ID VARCHAR(20),
+    Snapshot_Date DATE,
+    Opening_Stock INT,
+    Stock_In INT,
+    Stock_Out INT,
+    Closing_Stock INT,
+    Reorder_Level INT,
+    Safety_Stock INT,
+    Inventory_Value_INR DECIMAL(15,2),
+    Stock_Status VARCHAR(50),
+    Capacity_Utilization_Percent DECIMAL(5,2)
 );
